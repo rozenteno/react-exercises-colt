15431a1901e03528d02653777ab5b738b7895bf4
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2019-07-18 00:14:27 -0500

    Added project flip a coin

---
+++ flip-coin/src/CoinContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { choice } from "./helpers";
+import Coin from "./Coin";
+
+export default class CoinContainer extends Component {
+  static defaultProps = {
+    coins: [
+      { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+      { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+    ]
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currCoin: null,
+      nFlips: 0,
+      nHeads: 0,
+      nTails: 0
+    };
+  }
+  handleClick = e => {
+    const newCoin = choice(this.props.coins);
+    this.setState(st => {
+      return {
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="CoinContainer">
+        <h2>Let's Flip a Coin!</h2>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Flip Coin!</button>
+        <p>
+          Out of {this.state.nFlips} flips, there have been {this.state.nHeadss}{" "}
+          heads and {this.state.nTails} tails
+        </p>
+      </div>
+    );
+  }
+}
+
+// heads https://tinyurl.com/react-coin-heads-jpg
+// tails https://tinyurl.com/react-coin-tails-jpg
